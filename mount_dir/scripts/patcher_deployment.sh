2394468594adcc039f9ac978a698fb395753c609
Eszter Dudas-Schveder <172996984@liferay.com>
2021-02-16 18:15:55 +0100

    WIP - Mount these in the container

---
+++ mount_dir/scripts/patcher_deployment.sh
@@ -0,0 +1,19 @@
+# This script creates another script under /usr/local/liferay/scripts/pre-startup
+# which deploys osb-patcher-portlet before startup.
+mkdir -p /usr/local/liferay/scripts/pre-startup
+cat << EOF > /usr/local/liferay/scripts/pre-startup/0_deploy_patcher.sh
+#!/bin/bash
+export ANT_HOME=/mnt/liferay/ant
+export PATH=\$ANT_HOME/bin:\$PATH
+export USER=liferay
+echo -e "ANT_HOME: \$ANT_HOME\nPATH: \$PATH\nUSER: \$USER"
+printf "    app.server.parent.dir=/opt/liferay\n    app.server.tomcat.dir=\\\${app.server.parent.dir}/tomcat\n    ant.build.javac.source=1.7\n    ant.build.javac.target=1.7" > /mnt/liferay-plugins-ee/build.\$USER.properties
+cd /mnt/liferay-plugins-ee/shared/osb-patcher-shared ; ant deploy
+cd /mnt/liferay-plugins-ee/hooks/portal-compat-hook ; ant deploy
+cd /mnt/liferay-plugins-ee/portlets/marketplace-portlet ; ant deploy
+cd /mnt/liferay-plugins-ee/themes/osb-patcher-theme ; ant deploy
+cd /mnt/liferay-plugins-ee/portlets/osb-patcher-portlet ; ant deploy
+cp /opt/liferay/license-portaldevelopment-developer-6.2ga1-liferaycom.xml /opt/liferay/deploy/license-portaldevelopment-developer-6.2ga1-liferaycom.xml
+EOF
+
+chmod u+x /usr/local/liferay/scripts/pre-startup/0_deploy_patcher.sh
